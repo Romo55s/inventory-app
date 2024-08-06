98990977df7e7649e4c4451d53b70575cd5e0cc7
Romo55s <pp.tony777@gmail.com>
2024-08-05 18:48:57 -0600

    removing the conver.ts old and updating the styles for the page and TextCard still need to be improve

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import TextCard from "@/components/cards/TextCard";
 export default function Home() {
   const imgInputRef = useRef<HTMLInputElement | null>(null);
   const [processing, setProcessing] = useState<boolean>(false);
-  const [texts, setTexts] = useState<string[]>([]); 
+  const [texts, setTexts] = useState<string[]>([]);
   const [imageUrls, setImageUrls] = useState<string[]>([]);
 
   const openBrowse = () => {
@@ -34,24 +34,24 @@ export default function Home() {
   };
 
   const processFiles = async (files: File[]) => {
-    const urls = files.map(file => URL.createObjectURL(file));
-    setImageUrls(prevUrls => [...prevUrls, ...urls]);
+    const urls = files.map((file) => URL.createObjectURL(file));
+    setImageUrls((prevUrls) => [...prevUrls, ...urls]);
 
     setProcessing(true);
     try {
       const formData = new FormData();
-      files.forEach(file => formData.append('imagePath', file));
+      files.forEach((file) => formData.append("imagePath", file));
 
-      const response = await fetch('/api/process-image', {
-        method: 'POST',
+      const response = await fetch("/api/process-image", {
+        method: "POST",
         body: formData,
       });
 
       const data = await response.json();
-      console.log(data.results); 
-      setTexts(prevTexts => [...prevTexts, ...data.results]); // Agregar los nuevos resultados al array existente
+      console.log(data.results);
+      setTexts((prevTexts) => [...prevTexts, ...data.results]); // Agregar los nuevos resultados al array existente
     } catch (error) {
-      console.error('Error processing images:', error);
+      console.error("Error processing images:", error);
     } finally {
       setProcessing(false);
     }
@@ -59,43 +59,54 @@ export default function Home() {
 
   return (
     <main className="text-white">
-      <h2 className="px-5 pt-10 text-center md:text-6xl text-3xl font-[800]">
-        Upload the{" "}
-        <span className="bg-gradient-to-r from-blue-500 to-green-500 text-transparent bg-clip-text">
-          images
-        </span>
-      </h2>
-      <input
-        type="file"
-        hidden
-        ref={imgInputRef}
-        required
-        multiple
-        onChange={handleFileChange}
-      />
-      <div className="w-full md:px-20 p-5 flex items-center justify-center cursor-pointer">
-        <div
-          onClick={openBrowse}
-          onDrop={handleDrop}
-          onDragOver={(e) => e.preventDefault()}
-          className="bg-slate-900 min-h-[50vh] w-full p-5 rounded-xl flex items-center justify-center"
-        >
-          <div className="flex items-center justify-center flex-col">
-            <p className="text-center text-3xl font-[700] text-slate-700">
-              {processing
-                ? "Processing Images ..."
-                : "Browse Or Drop Your Images Here"}
-            </p>
-            <span className="text-[150px] text-slate-700">
-              <CiImageOn className={processing ? "animate-pulse" : ""} />
+      <div className="flex flex-col md:flex-row">
+        <div className="flex flex-col w-full md:w-1/2">
+          <h2 className="px-5 pt-10 text-center md:text-6xl text-3xl font-[800]">
+            Upload the{" "}
+            <span className="bg-gradient-to-r from-blue-500 to-green-500 text-transparent bg-clip-text">
+              images
             </span>
+          </h2>
+          <input
+            type="file"
+            hidden
+            ref={imgInputRef}
+            required
+            multiple
+            onChange={handleFileChange}
+          />
+          <div className="w-full md:px-20 p-5 flex items-center justify-center cursor-pointer">
+            <div
+              onClick={openBrowse}
+              onDrop={handleDrop}
+              onDragOver={(e) => e.preventDefault()}
+              className="bg-slate-900 min-h-[50vh] w-full p-5 rounded-xl flex items-center justify-center"
+            >
+              <div className="flex items-center justify-center flex-col">
+                <p className="text-center text-3xl font-[700] text-slate-700">
+                  {processing
+                    ? "Processing Images ..."
+                    : "Browse Or Drop Your Images Here"}
+                </p>
+                <span className="text-[150px] text-slate-700">
+                  <CiImageOn className={processing ? "animate-pulse" : ""} />
+                </span>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="my-10 md:px-20 px-5">
-        {texts.map((text, index) => (
-          <TextCard key={index} i={index} t={text} imageUrl={imageUrls[index] || ""} />
-        ))}
+        <div className="w-full md:w-1/2">
+          <div className="my-10 md:px-20 px-5 h-[80vh] overflow-y-auto">
+            {texts.map((text, index) => (
+              <TextCard
+                key={index}
+                i={index}
+                t={text}
+                imageUrl={imageUrls[index] || ""}
+              />
+            ))}
+          </div>
+        </div>
       </div>
     </main>
   );
--- src/components/cards/TextCard.tsx
+++ src/components/cards/TextCard.tsx
@@ -1,37 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
 
-const TextCard = ({ t, i, imageUrl }: { t: string; i: number; imageUrl: string }) => {
+const TextCard = ({
+  t,
+  i,
+  imageUrl,
+}: {
+  t: string;
+  i: number;
+  imageUrl: string;
+}) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const copyToClipBoard = (text: string) => {
     navigator.clipboard.writeText(text);
   };
+
   return (
-    <div className="">
-      <div className="flex w-full items-center justify-between mb-5 px-5">
-        <p className="text-xl font-[600]">
-          {`(${i + 1}) `}
-          {new Date().toLocaleString("en-US", {
-            timeZone: "America/Mexico_City",
-          })}
-        </p>
-        <button
-          onClick={() => {
-            copyToClipBoard(t);
-          }}
-          className="bg-white text-black text-sm md:text-base rounded-md px-5 py-2 transition-all hover:text-white hover:bg-slate-900"
-        >
-          Copy
-        </button>
+    <>
+      <div
+        className="rounded-lg shadow-md p-4 bg-gray-900 cursor-pointer mb-5"
+        onClick={() => setIsModalOpen(true)}
+      >
+        <div className="flex justify-between items-center mb-2">
+          <p className="text-sm font-semibold">
+            {`(${i + 1}) `}
+            {new Date().toLocaleString("en-US", {
+              timeZone: "America/Mexico_City",
+            })}
+          </p>
+          <button
+            onClick={(e) => {
+              e.stopPropagation();
+              copyToClipBoard(t);
+            }}
+            className="bg-gray-200 text-black text-xs rounded-md px-3 py-1 transition-all hover:text-white hover:bg-gray-800"
+          >
+            Copy
+          </button>
+        </div>
+        <div className="flex items-center gap-2">
+          {imageUrl && (
+            <img
+              src={imageUrl}
+              alt="Uploaded"
+              className="w-[8vh] h-[8vh] rounded-md"
+            />
+          )}
+          <input
+            className="w-auto p-1 text-sm outline-none bg-gray-800 rounded-md"
+            defaultValue={t}
+            type="text"
+          />
+        </div>
       </div>
-      <div>
-        {imageUrl && (
-          <img src={imageUrl} alt="Uploaded" className="w-[40vh] h-[40vh] mb-10" />
-        )}
-      </div>
-      <textarea
-        className="w-full p-5 min-h-[30vh] outline-none bg-slate-900 rounded-xl"
-        defaultValue={t}
-      ></textarea>
-    </div>
+
+      {isModalOpen && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+          <div className="bg-gray-900 rounded-lg shadow-lg p-6 w-3/4 max-w-8xl">
+            <div className="flex justify-between items-center mb-4">
+              <p className="text-lg font-semibold">
+                {`(${i + 1}) `}
+                {new Date().toLocaleString("en-US", {
+                  timeZone: "America/Mexico_City",
+                })}
+              </p>
+              <button
+                onClick={() => setIsModalOpen(false)}
+                className="bg-gray-200 text-black text-xs rounded-md px-3 py-1 transition-all hover:text-white hover:bg-gray-800"
+              >
+                Close
+              </button>
+            </div>
+            <div className="flex items-center gap-4">
+              {imageUrl && (
+                <img
+                  src={imageUrl}
+                  alt="Uploaded"
+                  className="w-[80vh] h-[80vh] rounded-md"
+                />
+              )}
+              <input
+                className="w-full p-2 text-lg outline-none bg-gray-800 rounded-md"
+                defaultValue={t}
+                type="text"
+              />
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
