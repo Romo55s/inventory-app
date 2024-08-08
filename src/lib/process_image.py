4fdad813809983e1225e11db4cd6a523ea5b496b
Romo55s <pp.tony777@gmail.com>
2024-08-08 14:26:41 -0600

    Adding the env for the API_KEY

--- src/lib/process_image.py
+++ src/lib/process_image.py
@@ -1,19 +1,27 @@
-# src/lib/process_image.py
-import sys
-import time
-import easyocr
 import json
 import google.generativeai as genai
+import time
+import sys
+from dotenv import load_dotenv
+import os
 
-genai.configure(api_key='AIzaSyBLm5Xpxgr-5lS6HfNUXj9s4GUXkEIjr0U')
+# Load environment variables from .env file
+load_dotenv()
+
+# Get the API key from environment variables
+api_key = os.getenv('API_KEY')
+if not api_key:
+    raise ValueError("API key not found in environment variables")
+
+genai.configure(api_key=api_key)
 
 def process_image(image_path):
     start_time = time.time()
     imgToProcess = genai.upload_file(path=image_path)
     model = genai.GenerativeModel("gemini-1.5-flash")
     response = model.generate_content(
-[imgToProcess, "\n\n",
-    "You will receive an image that might contain a serial number, possibly with visual noise or overlaid with other elements. If you identify a serial number, extract it accurately. In case it's an image of a car, it might be at different angles, with parts obscured, partial logos or with brands of other companies. Prioritize the identification and extraction of the main vehicle brand, considering that the logo might be in different locations of the car (hood, trunk, wheels, etc.). If the serial number is alphanumeric, specify the format (e.g., XXX-1234567). If the image is of low quality, indicate if extraction is possible or if a higher resolution image is required.Analyze the image and provide the following information in a concise format:* **Serial number:** If you find a serial number, extract it and specify if it's alphanumeric. If no serial number is found, respond: SERIAL_NUMBER_NULL* **Vehicle brand:** If you identify the brand, indicate it. If not found, respond: BRAND_NULL* **Vehicle model:** Extract the vehicle model. If not found, respond: MODEL_NULL* **Output format:*** If only the serial number is found: SERIAL_NUMBER* If only the brand is found: BRAND* If only the model is found: MODEL* If both brand and model are found: MODEL BRAND (e.g., COROLLA TOYOTA)"]
+        [imgToProcess, "\n\n",
+        "You will receive an image that might contain a serial number, possibly with visual noise or overlaid with other elements. If you identify a serial number, extract it accurately. In case it's an image of a car, it might be at different angles, with parts obscured, partial logos or with brands of other companies. Prioritize the identification and extraction of the main vehicle brand, considering that the logo might be in different locations of the car (hood, trunk, wheels, etc.). If the serial number is alphanumeric, specify the format (e.g., XXX-1234567). If the image is of low quality, indicate if extraction is possible or if a higher resolution image is required.Analyze the image and provide the following information in a concise format:* **Serial number:** If you find a serial number, extract it and specify if it's alphanumeric. If no serial number is found, respond: SERIAL_NUMBER_NULL* **Vehicle brand:** If you identify the brand, indicate it. If not found, respond: BRAND_NULL* **Vehicle model:** Extract the vehicle model. If not found, respond: MODEL_NULL* **Output format:*** If only the serial number is found: SERIAL_NUMBER* If only the brand is found: BRAND* If only the model is found: MODEL* If both brand and model are found: MODEL BRAND (e.g., COROLLA TOYOTA)"]
     )
     
     # Check if the response was blocked
